50507c1f319d499e711f5ff18057f7ec0ca1458c
Mika Arai <araim330@newschool.edu>
2025-05-07 15:47:18 -0400

    updated index and css; plus java filter function

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+let landingImg = document.querySelector(".hero-image");
+let projectTitle = document.querySelector("#project-title");
+let subtitle = document.querySelector(".subtitle");
+let repository = document.querySelector(".repository");
+let viewWebsite = document.querySelector(".view-website");
+
+
+function changeContent(let projectNumber){
+	if(projectNumber = 1){
+	landingImg.src = “assets/img1.png”;
+	}
+  }
