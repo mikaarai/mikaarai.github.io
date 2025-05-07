6c1da52e0e0a2fce39869299866be9c8dc77dc36
Mika Arai <araim330@newschool.edu>
2025-05-07 18:20:49 -0400

    remove footer, updated img, updated js filter

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,12 @@ let repository = document.querySelector(".repository");
 let viewWebsite = document.querySelector(".view-website");
 
 
-function changeContent(let projectNumber){
-	if(projectNumber = 1){
-	landingImg.src = “assets/img1.png”;
+function changeContent(projectNumber){
+	if(projectNumber == 1){
+		landingImg.src = "assets/images/function-img.png";
+		projectTitle.textContent = "FUNCTION";
+		subtitle.textContent = "TBD";
+		repository.href = "https://github.com/mikaarai/functions";
+		viewWebsite.href = "https://mikaarai.github.io/function/";
 	}
   }
