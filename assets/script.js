153f26bd0f969c455c3c63dcdfcb51f585fa54a2
Mika Arai <araim330@newschool.edu>
2025-05-07 20:20:11 -0400

    added details shown when nav button is clicked

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// function
+// function for side nav
 let landingImg = document.querySelector(".hero-image");
 let projectTitle = document.querySelector("#project-title");
 let subtitle = document.querySelector(".subtitle");
@@ -7,24 +7,45 @@ let viewWebsite = document.querySelector(".view-website");
 
 
 
-// filter for each page
+// details shown on each page
 function changeContent(projectNumber){
 	if(projectNumber == 1){
 		landingImg.src = "assets/images/function-img.png";
-		projectTitle.textContent = "FUNCTION";
+		projectTitle.textContent = "FUNCTIONS";
 		subtitle.textContent = "TBD";
 		repository.href = "https://github.com/mikaarai/functions";
-		viewWebsite.href = "https://mikaarai.github.io/function/";
+		viewWebsite.href = "https://mikaarai.github.io/functions/";
+	  } else if (projectNumber == 2) {
+		landingImg.src = "assets/images/links-img.png";
+		projectTitle.textContent = "LINKS";
+		subtitle.textContent = "TBD";
+		repository.href = "https://github.com/mikaarai/links";
+		viewWebsite.href = "https://mikaarai.github.io/links/";
+	  } else if (projectNumber == 3) {
+		landingImg.src = "assets/images/binding-img.png";
+		projectTitle.textContent = "BINDING";
+		subtitle.textContent = "TBD";
+		repository.href = "https://github.com/mikaarai/binding";
+		viewWebsite.href = "https://mikaarai.github.io/binding/";
+	  } else if (projectNumber == 4) {
+		landingImg.src = "assets/images/manuscript.png";
+		projectTitle.textContent = "MANUSCRIPT";
+		subtitle.textContent = "TBD";
+		repository.href = "https://github.com/mikaarai/manuscript";
+		viewWebsite.href = "https://mikaarai.github.io/manuscript/";
 	}
   }
 
-  function setActive(button) {
-	// remove "active" from all buttons
-	// to easily see which page you are in
-	// resource: https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+
+// active state
+// to easily see which page you are in
+// resource: https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+
+// remove "active" from all buttons
+function setActive(button) {
 	const buttons = document.querySelectorAll(".menu button");
 	buttons.forEach(btn => btn.classList.remove("active"));
   
-	// add "active" to the clicked button
+// add "active" to the clicked button
 	button.classList.add("active");
   }
